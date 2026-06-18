b82522e2df2f663bb7e7564b3fcfe41caad6cc39
九觅 <2028207369@qq.com>
2026-06-18 15:29:49 +0800

    feat: wire up GSAP plugin and animations.css in main.ts

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,6 +4,8 @@ import naive from 'naive-ui'
 import router from './router'
 import App from './App.vue'
 import './styles/global.css'
+import '@/plugins/gsap'              // GSAP + ScrollTrigger one-time registration
+import '@/styles/animations.css'     // Animation CSS variables + initial states
 
 const app = createApp(App)
 
